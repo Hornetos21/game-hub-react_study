bf5327c867bbac4411bf4974def119b0ce10a189
Igor Shapovalov <kizore5@gmail.com>
2023-05-03 11:39:53 +1000

    Implement infinite queries

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,15 +1,16 @@
 import { useQuery } from '@tanstack/react-query'
 import genres from '../data/genres'
-import apiClient, { FetchResponse } from '../services/api-client'
 import { Genre } from '../types'
+import APIClient from '../services/api-client'
+
+const apiGenres = new APIClient<Genre>('/genres')
 
 // const useGenres = () => useData<Genre>('/genres')
 // const useGenres = () => ({ data: genres, isLoading: false, error: null })
 const useGenres = () =>
   useQuery({
     queryKey: ['genres'],
-    queryFn: () =>
-      apiClient.get<FetchResponse<Genre>>('/genres').then((res) => res.data),
+    queryFn: apiGenres.getAll,
     staleTime: 24 * 60 * 60 * 1000, // 24 hours
     initialData: { count: genres.length, results: genres },
   })
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,16 +1,30 @@
-import axios, { CanceledError } from 'axios'
+import axios, { AxiosRequestConfig } from 'axios'
 
 export interface FetchResponse<T> {
   count: number
+  next: string | null
   results: T[]
 }
 
 const API_KEY: string = import.meta.env.VITE_API_KEY
-export default axios.create({
+
+const axiosInstance = axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
     key: API_KEY,
   },
 })
 
-export { CanceledError }
+class APIClient<T> {
+  endpoint: string
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint
+  }
+  getAll = (config: AxiosRequestConfig) =>
+    axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data)
+}
+
+export default APIClient
